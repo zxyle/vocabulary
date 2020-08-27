250e5989a5dd903f0d169d667558838ed236f5e1
zxyle <zxyful@gmail.com>
2020-08-27 11:14:06 +0800

    update code

--- generate.py
+++ generate.py
@@ -2,15 +2,10 @@ import string
 
 with open("./words.txt") as f1, open("./README.md", "w") as f2:
     lines = f1.readlines()
-    non_repeating_words = {line.strip() for line in lines}
-
-    # sorted
-    words = list(non_repeating_words)
+    words = list({line.strip() for line in lines})
     words.sort()
 
-    # write file
     f2.write("# Computer Science Vocabulary\n")
-
     for alphabet in string.ascii_uppercase:
         f2.write(f"\n## {alphabet}\n")
 
--- update.sh
+++ update.sh
@@ -6,11 +6,13 @@ if [ -z $(git status README.md --porcelain) ];
 then
     echo "README.md IS NOT MODIFIED"
 else
-    echo "PLEASE COMMIT YOUR CHANGE FIRST!!!"
+    echo "START SUBMIT"
     echo git status
     git config --global user.email "zxyful@gmail.com"
     git config --global user.name "zxyle"
     git add README.md
     git commit -m "Update README.md"
+    echo "COMMIT SUCCESS"
     git push
+    echo "PUSH SUCCESS"
 fi
